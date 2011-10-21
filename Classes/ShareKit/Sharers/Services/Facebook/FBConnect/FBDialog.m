30238f6fc56c0fd365ea508020ae88069eb498e7
Robin Hos <robin.hos@gmail.com>
2011-10-21 17:11:15 +0200

    [FIX] Getting NSURLErrorDomain/-999 errors

--- Classes/ShareKit/Sharers/Services/Facebook/FBConnect/FBDialog.m
+++ Classes/ShareKit/Sharers/Services/Facebook/FBConnect/FBDialog.m
@@ -440,7 +440,8 @@ BOOL FBIsDeviceIPad(void) {
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
   // 102 == WebKitErrorFrameLoadInterruptedByPolicyChange
-  if (!([error.domain isEqualToString:@"WebKitErrorDomain"] && error.code == 102)) {
+  if (!(([error.domain isEqualToString:@"NSURLErrorDomain"] && error.code == -999) ||
+        ([error.domain isEqualToString:@"WebKitErrorDomain"] && error.code == 102))) {
     [self dismissWithError:error animated:YES];
   }
 }
